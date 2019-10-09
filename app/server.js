0c3c5fb652a081ac33b967e71c783141f4f3d252
Tóth László <ttsz84@gmail.com>
2019-10-09 21:15:54 +0200

    Logging enabled

--- app/server.js
+++ app/server.js
@@ -1,22 +1,30 @@
+var log4js = require('log4js');
+log4js.configure('app/config/log4js.json');
 var express = require("express");
 var app = express();
 
 var express = require("express");
 var app = express();
 var converter = require("./converter");
+var log = log4js.getLogger("app");
+
+app.use(log4js.connectLogger(log4js.getLogger("http"), { level: 'auto' }));
 
 app.get("/rgbToHex", function(req, res) {
   let red   = parseInt(req.query.red, 10);
   let green = parseInt(req.query.green, 10);
   let blue  = parseInt(req.query.blue, 10);
+  log.debug(`Requested parameters : ${red}, ${green}, ${blue}`);
   let hex = converter.rgbToHex(red, green, blue);
-
+  log.debug(`response : ${hex}`);
   res.send(hex);
 });
   
 app.get("/hexToRgb", function(req, res) {
   let hex = req.query.hex;
+  log.debug(`Requested parameter : ${hex}`);
   let rgb = converter.hexToRgb(hex);
+  log.debug(`response : ${JSON.stringify(rgb)}`);
   res.send(JSON.stringify(rgb));
 });
 
@@ -25,3 +33,4 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000);
+log.info('Express server listening on port 3000');
